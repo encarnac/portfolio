c08c9aba98ad449d2876555782281c2812f328b2
Colene <coleneencarnado@gmail.com>
2024-01-26 15:33:29 -0800

    Added animation to navbar and state color change

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function App() {
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
   const [theme, setTheme] = useState("dark");
+  const [selectedPage, setSelectedPage] = useState("hero");
 
   const toggleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
+
+  // const selectPage = (page) => {
+  //   setSelectedPage(page.title);
+  // };
 
   useEffect(() => {
     if (theme === "dark") {
@@ -18,12 +25,36 @@ export default function App() {
     }
   }, [theme]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col bg-gradient-to-tr from-[#B0BEDF] via-[#D0C5DD] to-[#E1C6D7] dark:from-[#10031A] dark:via-[#18082C] dark:to-[#14022A]">
-      <Navbar theme={theme} toggleTheme={toggleTheme} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+        theme={theme}
+        toggleTheme={toggleTheme}
+      />
       <div className="container mx-auto mt-24 px-12 py-4">
         <HeroSection />
-        <AboutSection />
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("about")}
+        >
+          <AboutSection />
+        </motion.div>
       </div>
     </main>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,37 @@
 import { useState } from "react";
+import { Link } from "./Link";
+import { NavDrawer } from "./NavDrawer";
+import { MoonIcon as MoonIconSolid } from "@heroicons/react/24/solid";
 import {
   Bars3Icon,
   XMarkIcon,
   CodeBracketSquareIcon,
   MoonIcon,
 } from "@heroicons/react/24/outline";
-import { MoonIcon as MoonIconSolid } from "@heroicons/react/24/solid";
-import { NavLink } from "./NavLink";
-import { NavDrawer } from "./NavDrawer";
 
-const pageSections = [
-  { title: "About", href: "#about" },
-  { title: "Projects", href: "#projects" },
-  { title: "Contact", href: "#contact" },
+const pages = [
+  { title: "about", href: "#about" },
+  { title: "projects", href: "#projects" },
+  { title: "contact", href: "#contact" },
 ];
 
-const Navbar = ({ theme, toggleTheme }) => {
+const Navbar = ({
+  isTopOfPage,
+  theme,
+  toggleTheme,
+  selectedPage,
+  setSelectedPage,
+}) => {
   const [navbarOpen, setNavbarOpen] = useState(false);
+  const navbarBgColor = isTopOfPage
+    ? "border-b dark:border-[#220C3D] border-[#d9dbdf]"
+    : "bg-[#E9EFFD] dark:bg-[#220C3D]";
+
   return (
-    <nav className="backdrop-blur fixed left-0 right-0 top-0 z-10 bg-[#b8cee8] bg-opacity-20 dark:bg-[#14022A] dark:bg-opacity-90">
-      <div className="container mx-auto flex flex-wrap items-center justify-between px-4 py-4">
+    <nav
+      className={`${navbarBgColor} backdrop-blur-md fixed left-0 right-0 top-0 z-10 `}
+    >
+      <div className="border-[#d9dbdf] container mx-auto flex flex-wrap items-center justify-between px-4 py-4">
         <a
           href="/"
           className="text-2xl font-semibold text-stone-600 dark:text-white md:text-4xl"
@@ -30,7 +42,7 @@ const Navbar = ({ theme, toggleTheme }) => {
         <div className="mobile-menu flex flex-row text-stone-500 dark:text-[#b7b0c2] md:hidden">
           <button
             onClick={toggleTheme}
-            className="p-0 hover:text-stone-600 dark:hover:text-white"
+            className="transition duration-500 p-0 hover:text-[#DC659C]  dark:text-[#6a3dd1] dark:hover:text-white"
           >
             {theme === "dark" ? (
               <MoonIconSolid className="h-5 w-5" />
@@ -41,32 +53,34 @@ const Navbar = ({ theme, toggleTheme }) => {
           {navbarOpen ? (
             <button
               onClick={() => setNavbarOpen(false)}
-              className="flex items-center rounded px-3 py-2 hover:text-stone-600 dark:hover:text-white"
+              className="flex items-center rounded px-3 py-2 transition duration-500 hover:text-[#DC659C] dark:hover:text-[#6a3dd1]"
             >
               <XMarkIcon className="h-5 w-5" />
             </button>
           ) : (
             <button
               onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 hover:text-stone-600 dark:hover:text-white"
+              className="flex items-center px-3 py-2 transition duration-500 hover:text-[#DC659C] dark:hover:text-[#6a3dd1]"
             >
               <Bars3Icon className="h-5 w-5" />
             </button>
           )}
         </div>
-        <div
-          className="menu hidden text-stone-500 dark:text-[#b7b0c2] md:block md:w-auto"
-          id="navbar"
-        >
+        <div className="menu hidden  md:block md:w-auto" id="navbar">
           <ul className="flex justify-evenly p-4 md:flex-row md:space-x-8 md:p-0">
-            {pageSections.map((link, index) => (
+            {pages.map((page, index) => (
               <li className={index}>
-                <NavLink href={link.href} title={link.title}></NavLink>
+                {/* <NavLink href={link.href} title={link.title}></NavLink> */}
+                <Link
+                  page={page}
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
               </li>
             ))}
             <button
               onClick={toggleTheme}
-              className="p-0 hover:text-stone-600 dark:hover:text-white"
+              className="p-0 hover:text-[#DC659C] text-stone-500 transition duration-500 dark:text-[#6a3dd1] dark:hover:text-white"
             >
               {theme === "dark" ? (
                 <MoonIconSolid className="h-5 w-5" />
@@ -77,7 +91,13 @@ const Navbar = ({ theme, toggleTheme }) => {
           </ul>
         </div>
       </div>
-      {navbarOpen ? <NavDrawer links={pageSections} /> : null}
+      {navbarOpen ? (
+        <NavDrawer
+          pages={pages}
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+      ) : null}
     </nav>
   );
 };
