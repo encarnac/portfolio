cdd572ddb7d5784b7f661e5728bdebd5df3c590f
Colene <coleneencarnado@gmail.com>
2024-01-26 18:18:58 -0800

    Changed order of AboutMe items in mobile

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,12 +3,12 @@ import AboutPhoto from "../assets/about-pfp.png";
 const AboutSection = ({ setSelectedPage }) => {
   return (
     <div id="about" className="my-72 grid grid-cols-1 lg:grid-cols-12">
-      <div className="col-span-5 place-self-center">
+      <div className="order-last col-span-5 place-self-center md:order-first">
         <div className="relative h-[250px] w-[250px] overflow-clip rounded-full bg-[#ad99db] dark:bg-[#6a3dd1] lg:h-[300px] lg:w-[300px]">
           <img src={AboutPhoto} />
         </div>
       </div>
-      <div className="col-span-7 mr-0 mt-12 place-self-center rounded-3xl bg-gradient-to-br from-[#68A3EB] to-[#F38BBB] px-1 py-1 text-center shadow-sm dark:from-[#3034C2] dark:via-[#6A3DD1] dark:to-[#9329BE] lg:mr-16 lg:mt-0 lg:text-left">
+      <div className="col-span-7 mb-12 mr-0 place-self-center rounded-3xl bg-gradient-to-br from-[#68A3EB] to-[#F38BBB] px-1 py-1 text-center shadow-sm dark:from-[#3034C2] dark:via-[#6A3DD1] dark:to-[#9329BE] lg:mb-0 lg:mr-16 lg:text-left">
         <div className="h-fit rounded-3xl bg-[#d2c6da] px-8 py-10 text-[#57545c] dark:bg-[#14022A] dark:text-indigo-100 lg:m-1 lg:px-16 lg:py-24">
           {" "}
           <h1 className="mb-6 text-2xl font-extrabold md:text-3xl">About Me</h1>
