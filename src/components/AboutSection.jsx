c944c87a2e433a45e54f3e290569649239baa1a1
Colene <coleneencarnado@gmail.com>
2024-01-27 01:13:23 -0800

    Created new layout for AboutSection with tabs

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -10,46 +10,50 @@ const motionVariants = (direction) => {
 
 const AboutSection = ({ setSelectedPage }) => {
   return (
-    <div id="about" className="mb-48 grid grid-cols-1 gap-x-0 md:grid-cols-12">
-      <div className="order-last col-span-5 place-self-center md:order-first">
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ type: "tween", duration: 0.5 }}
-          variants={motionVariants("right")}
-        >
-          <div className="relative h-[250px] w-[250px] overflow-clip rounded-full bg-[#ad99db] dark:bg-[#6a3dd1] lg:h-[300px] lg:w-[300px]">
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.5 }}
+      transition={{ type: "tween", duration: 0.5 }}
+      variants={motionVariants("right")}
+    >
+      <div
+        id="about"
+        className="md:grid-rows-8 mb-48 grid grid-flow-row grid-cols-1 items-stretch justify-items-center gap-x-0 gap-y-4 md:grid-cols-12 md:justify-items-stretch md:px-28"
+      >
+        <div className="px-2 md:col-span-7 md:row-span-1">
+          <h1 className="text-2xl font-extrabold dark:text-white md:text-3xl">
+            About Me
+          </h1>
+        </div>
+
+        <div className="md:row-span-8 order-last place-self-center md:order-first md:col-span-5">
+          <div className="h-fill relative max-w-[300px] overflow-clip rounded-full bg-[#ad99db] dark:bg-[#6a3dd1]">
             <img src={AboutPhoto} />
           </div>
-        </motion.div>{" "}
-      </div>
-      <div className="col-span-7">
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ type: "tween", duration: 0.5 }}
-          variants={motionVariants("left")}
-        >
-          <div className="m-1 mb-6 max-w-xl basis-1/2 place-self-center rounded-3xl bg-gradient-to-br from-[#68A3EB] to-[#F38BBB] px-1 py-1 text-center shadow-sm dark:from-[#3034C2] dark:via-[#6A3DD1] dark:to-[#9329BE] md:mx-6 lg:mx-12 lg:my-6 lg:text-left">
-            <div className="h-fit rounded-3xl bg-[#d7cfdc] px-4 py-8 text-[#57545c] dark:bg-[#14022A] dark:text-indigo-100 lg:m-1 lg:px-8 lg:py-10">
-              <h1 className="mb-6 text-2xl font-extrabold md:text-3xl">
-                About Me
-              </h1>
-              <p className="text-sm lg:text-base">
-                I am a full stack web developer with a passion for creating
-                interactive and responsive web applications. I have experience
-                working with JavaScript, React, Node.js, Express, MySQL,
-                MongoDB, HTML, CSS (TailwindCSS and Bootstrap), and Git. I am a
-                quick learner and I am always looking to expand my knowledge and
-                skill set.
-              </p>
-            </div>
+        </div>
+        <div className="md:row-span-7 place-self-stretch px-2 dark:text-white md:col-span-7">
+          <div className="mb-4 flex flex-row justify-start">
+            <button className="mr-1 grow pb-1 hover:border-b-2 hover:border-b-[#3034C2] md:text-start">
+              My Story
+            </button>
+            <button className="mr-1 grow pb-1 hover:border-b-2 hover:border-b-[#3034C2] md:text-start">
+              Education
+            </button>
+            <button className="mr-1 grow pb-1 hover:border-b-2 hover:border-b-[#3034C2] md:text-start">
+              Skills
+            </button>
           </div>
-        </motion.div>
+          <p className="mt-2 text-sm lg:text-base">
+            I am a full stack web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with JavaScript, React, Node.js, Express, MySQL, MongoDB,
+            HTML, CSS (TailwindCSS and Bootstrap), and Git. I am a quick learner
+            and I am always looking to expand my knowledge and skill set.
+          </p>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
