25cf8260f6af0e2ba0003cbee8bc3d24cb6b177e
Colene Encarnado <encarnac@oregonstate.edu>
2024-01-30 22:59:08 -0800

    Fixed transition duration to match

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -14,7 +14,7 @@ export const Link = ({ mobile, page, selectedPage, setSelectedPage }) => {
           selectedPage === page.title
             ? "dark:text-[#7943ed] text-[#DC659C]"
             : "text-stone-500 dark:text-[#b7b0c2]"
-        } transition duration-500 block rounded py-2 pl-3 pr-4 hover:text-[#DC659C] dark:hover:text-[#7943ed] sm:text-xl md:p-0`}
+        } transition duration-300 block rounded py-2 pl-3 pr-4 hover:text-[#DC659C] dark:hover:text-[#7943ed] sm:text-xl md:p-0`}
       >
         {page.title}
       </a>
