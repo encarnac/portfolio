35389b6a4928b96a56f05ff065294290350813ee
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-01 19:49:59 -0800

    Fixed alignment of About photo in diff tabs

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -27,7 +27,7 @@ const AboutSection = ({ setSelectedPage }) => {
       {" "}
       <div
         id="about"
-        className="grid grid-cols-1 items-center justify-items-center gap-x-0 py-28 lg:grid-cols-12"
+        className="grid grid-cols-1 items-start justify-items-center gap-x-0 py-2 lg:grid-cols-12"
       >
         <motion.div
           initial="hidden"
@@ -35,9 +35,9 @@ const AboutSection = ({ setSelectedPage }) => {
           viewport={{ once: true, amount: 0.6 }}
           transition={{ type: "tween", duration: 0.6 }}
           variants={motionVariants("right")}
-          className="flex flex-col items-center justify-items-center gap-y-2 lg:col-span-5"
+          className="flex flex-col items-center justify-items-center gap-y-2 py-0 lg:col-span-5 lg:py-14"
         >
-          <div className="size-[200px] sm:size-[250px] md:size-[275px] xl:size-[350px] relative my-2 rounded-full bg-[#F38BBB] dark:bg-[#7943ed]">
+          <div className="size-[200px] sm:size-[250px] md:size-[275px] xl:size-[350px] relative my-2 rounded-[100%] bg-[#F38BBB] dark:bg-[#7943ed]">
             <img src={AboutPhoto} className="absolute inset-x-0 -bottom-4" />
             <div className="absolute -right-4 top-24 w-fit rounded-br-2xl rounded-tl-2xl rounded-tr-2xl border-b-[2px] border-r-[2px] border-b-[#dfc1cf] border-r-[#b2cceb] bg-gradient-to-bl from-[#68A3EB]/60 to-[#F38BBB]/60 p-1 shadow-md backdrop-blur-2xl backdrop-filter dark:border-b-[#815b90] dark:border-r-[#6567a5] dark:from-[#3034C2] dark:via-[#6A3DD1] dark:to-[#9329BE] dark:shadow-xl dark:shadow-[#3a3d8d]/50">
               <span className="block break-words rounded-xl px-2 py-1 text-sm text-slate-200 transition duration-300">
@@ -58,7 +58,7 @@ const AboutSection = ({ setSelectedPage }) => {
           viewport={{ once: true, amount: 0.6 }}
           transition={{ type: "tween", duration: 0.8 }}
           variants={motionVariants("left")}
-          className="relative mt-8 flex h-full w-full min-w-fit flex-col items-center p-px py-6 lg:col-span-7 lg:mt-0 lg:min-w-min xl:w-11/12"
+          className="relative mt-4 flex h-full w-full min-w-fit flex-col items-center p-px py-6 lg:col-span-7 lg:mt-0 lg:min-w-min xl:w-11/12"
         >
           <div className="relative mt-1 flex w-max flex-row items-stretch justify-center border-b-[1px] border-b-[#726C7B]/40 px-8 transition dark:border-b-[#b7b0c2]/70 lg:mx-0">
             {AboutMeData.map((tabContent, index) => (
