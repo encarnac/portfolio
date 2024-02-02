b7c707b21d8417799330e1ca08e181bf2313f3dc
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-01 19:23:45 -0800

    Remvoed animation underline in navbar

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -2,8 +2,20 @@ import { motion } from "framer-motion";
 
 export const Link = ({ mobile, page, selectedPage, setSelectedPage }) => {
   const variants = {
-    default: { width: 0 },
-    active: { width: "calc(100% - 0.75rem)" },
+    hidden: {
+      height: 100,
+      width: 100,
+    },
+    visible: {
+      backgroundColor: ["#60F", "#09F", "#FA0"],
+      transition: {
+        delay: 1,
+        duration: 2,
+        ease: [0.075, 0.82, 0.165, 1],
+        repeat: Infinity,
+        repeatType: "reverse",
+      },
+    },
   };
   return (
     <div className="relative">
@@ -14,17 +26,10 @@ export const Link = ({ mobile, page, selectedPage, setSelectedPage }) => {
           selectedPage === page.title
             ? "dark:text-[#7943ed] text-[#DC659C]"
             : "text-stone-500 dark:text-[#b7b0c2]"
-        } transition duration-300 block rounded py-2 pl-3 pr-4 hover:text-[#DC659C] dark:hover:text-[#7943ed] sm:text-xl md:p-0`}
+        } transition ease-linear duration-300 block rounded py-2 pl-3 pr-4 hover:text-[#DC659C] dark:hover:text-[#7943ed] sm:text-xl md:p-0`}
       >
         {page.title}
       </a>
-      {page.title == selectedPage && !mobile ? (
-        <motion.div
-          layout
-          className="h-[2px] w-0 bg-[#DC659C] dark:bg-[#7943ed]"
-          animate={{ width: "100%" }}
-        />
-      ) : null}
     </div>
   );
 };
