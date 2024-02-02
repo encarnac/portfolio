687fd23b8f1d8d8ed757f4ffde065f728e3ab34e
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-02 11:54:56 -0800

    Fixed color of photo background in light mode

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -37,7 +37,7 @@ const AboutSection = ({ setSelectedPage }) => {
           variants={motionVariants("right")}
           className="flex flex-col items-center justify-items-center gap-y-2 py-0 lg:col-span-5 lg:py-14"
         >
-          <div className="size-[200px] sm:size-[250px] md:size-[275px] xl:size-[350px] before:size-[190px] sm:before:size-[250px] md:before:size-[255px] relative my-2 rounded-custom-1 bg-[#F38BBB] before:absolute before:right-3 before:rounded-custom-4 before:bg-[#7943ed] dark:bg-[#5A32AF] xl:before:h-[325px] xl:before:w-[335px]">
+          <div className="size-[200px] sm:size-[250px] md:size-[275px] xl:size-[350px] before:size-[190px] sm:before:size-[240px] md:before:size-[260px] relative my-2 rounded-custom-1 bg-[#afc6e2] before:absolute before:bottom-0 before:right-3 before:rounded-custom-4 before:bg-[#ad99db] dark:bg-[#5A32AF] dark:before:bg-[#7943ed] xl:before:h-[325px] xl:before:w-[335px]">
             <img src={AboutPhoto} className="absolute inset-x-0 -bottom-4" />
             <div className="absolute -right-4 top-24 w-fit rounded-br-2xl rounded-tl-2xl rounded-tr-2xl border-b-[2px] border-r-[2px] border-b-[#dfc1cf] border-r-[#b2cceb] bg-gradient-to-bl from-[#68A3EB]/60 to-[#F38BBB]/60 p-1 shadow-md backdrop-blur-2xl backdrop-filter dark:border-b-[#815b90] dark:border-r-[#6567a5] dark:from-[#3034C2] dark:via-[#6A3DD1] dark:to-[#9329BE] dark:shadow-xl dark:shadow-[#3a3d8d]/50">
               <span className="block break-words rounded-xl px-2 py-1 text-sm text-slate-200 transition duration-300">
