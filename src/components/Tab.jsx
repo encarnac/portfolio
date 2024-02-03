8cab904c6061160fb2349601783666dd04fadb2f
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-02 21:05:25 -0800

    Extended transition and shortened highlight

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -10,15 +10,15 @@ export const Tab = ({ active, selectTab, tabName }) => {
         active === tabName
           ? "text-[#6B6573] dark:text-[#CCC3D7]"
           : "dark:text-[#b7b0c2]/70 text-[#726C7B]/70   hover:text-[#6B6573] dark:hover:text-[#CCC3D7]"
-      }  transition duration-300 px-2 md:px-8 relative text-sm sm:text-base md:text-lg font-bold py-2`}
+      }  transition duration-300 px-2 md:px-4 relative text-sm sm:text-base md:text-lg font-bold py-2`}
     >
       <span className="relative">{tabName} </span>
 
       {active === tabName && (
         <motion.div
           layout
-          className="absolute -bottom-[.2rem] left-0 h-[.3rem] w-fit rounded-3xl bg-[#F38BBB] dark:bg-[#7943ed]"
-          animate={{ width: "100%", transition: { duration: 0.3 } }}
+          className="absolute -bottom-[.2rem] left-0 h-[.4rem] w-fit rounded-sm bg-[#F38BBB] dark:bg-[#7943ed]"
+          animate={{ width: "100%", transition: { duration: 0.4 } }}
         />
       )}
     </button>
