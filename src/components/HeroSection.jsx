768cd94c1ccedbf6ece2aa0ebdf3339e655d0983
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-02 16:59:44 -0800

    Margin resized for consistancy

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,8 +4,8 @@ import HeaderPhoto from "../assets/header-pfp.png";
 export default function Header() {
   return (
     <>
-      <section className="relative">
-        <div className="mb-48 grid grid-cols-1 lg:grid-cols-12 lg:gap-x-1">
+      <section className="relative mb-12">
+        <div className="grid grid-cols-1 lg:grid-cols-12 lg:gap-x-1">
           <div className="col-span-7 place-self-center text-center sm:text-left">
             <h1 className="mb-4 text-5xl font-extrabold text-[#857e8f] dark:text-white sm:text-6xl lg:text-7xl">
               <span className="bg-gradient-to-r from-[#68A3EB] to-[#F38BBB] bg-clip-text text-transparent dark:from-[#3034C2] dark:via-[#6A3DD1] dark:to-[#9329BE]">
