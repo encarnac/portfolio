59753a190b0fe5007577d41e13cd2464ab87b965
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-02 17:00:07 -0800

    Fixed tab size and font size for mobile

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -8,9 +8,9 @@ export const Tab = ({ active, selectTab, tabName }) => {
       onClick={() => selectTab(tabName)}
       className={`${
         active === tabName
-          ? "text-[#6F6977] dark:text-[#b7b0c2]"
-          : "dark:text-[#b7b0c2]/70 text-[#726C7B]/70   hover:text-[#726C7B] dark:hover:text-[#6F6977]"
-      }  transition duration-300  px-2 md:px-8 relative text-lg font-bold md:text-xl py-2`}
+          ? "text-[#5E5965] dark:text-[#C7BFD3]"
+          : "dark:text-[#b7b0c2]/70 text-[#726C7B]/70   hover:text-[#5E5965] dark:hover:text-[#C7BFD3]"
+      }  transition duration-300 px-px md:px-8 relative text-sm sm:text-base md:text-lg font-bold py-2`}
     >
       <span className="relative">{tabName} </span>
 
