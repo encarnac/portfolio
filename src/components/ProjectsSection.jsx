548876d62868c8e810cb7a26ba7cad770534647f
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-07 20:59:39 -0800

    Fixed Apple mobile view of project photos and added hover effects

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,7 +1,7 @@
 import watch_soon from "../assets/watch-soon.png";
 import adopt_a_pal from "../assets/adopt-a-pal.png";
 import portfolio from "../assets/portfolio.png";
-import { LuGithub, LuExternalLink } from "react-icons/lu";
+import { LuGithub, LuExternalLink, LuFigma } from "react-icons/lu";
 
 const ProjectsSection = () => {
   return (
@@ -16,10 +16,11 @@ const ProjectsSection = () => {
           </span>
         </h3>
       </div>
-      <div className="flex flex-col items-center space-y-8 text-[#3f3b43] dark:text-white">
+      <div className="space-y-8 text-[#3f3b43] dark:text-white">
         {/* FIRST PROJECT CARD */}
-        <div class="grid grid-cols-10 items-center">
-          <div class="col-span-full row-span-full p-3 px-4 text-start md:col-span-5 md:col-start-2 md:px-0">
+        <div class="group grid grid-cols-10 items-center">
+          {/* PROJECT DESCRIPTION */}
+          <div class="col-span-full row-span-full p-8 px-4 text-start opacity-0 transition duration-300 hover:opacity-100 md:col-span-5 md:col-start-2 md:p-0 md:opacity-100">
             <h4 class="font-mono text-sm">React Portfolio Website</h4>
             <h3 class="mb-2 mt-0 text-2xl font-bold">Portfolio</h3>
             <div class="">
@@ -40,38 +41,48 @@ const ProjectsSection = () => {
                 Framer Motion
               </li>
               <li className="rounded-lg border-2 border-[#797382]/40 px-2 py-px dark:border-[#807C8E]/90">
-                GCP
+                Netlify
               </li>
             </ul>
             <div className="mt-3 flex flex-row items-center justify-start space-x-4">
-              <a href="">
+              <a
+                href="https://github.com/encarnac/portfolio"
+                className="transition duration-300 ease-in-out hover:scale-110"
+              >
                 <LuGithub className="size-5" />
               </a>
-              <a href="">
+              <a
+                href="https://encarnac.netlify.app/"
+                className="transition duration-300 ease-in-out hover:scale-110"
+              >
                 <LuExternalLink className="size-5" />
               </a>
             </div>
           </div>
 
-          <div class="relative z-[-1] col-span-full row-span-full h-full rounded-2xl border-[1px] border-[#797382]/40 p-2 dark:border-[#807C8E]/50 md:col-start-6 md:col-end-10 md:h-fit">
+          {/* PROJECT PHOTO */}
+          <div class="relative z-[-1] col-span-full row-span-full h-full rounded-2xl border-[1px] border-[#797382]/40 border-white p-2 transition duration-300 ease-in-out group-hover:border-2 group-hover:border-[#eaa3c2] dark:border-[#807C8E]/70 dark:group-hover:border-[#7943ED] md:col-start-6 md:col-end-10 md:h-fit">
             <img
               src={portfolio}
               alt="adopt-a-pal mockups"
-              className="h-full rounded-xl opacity-30 md:opacity-100"
+              className="opacity30 h-full w-full rounded-xl object-cover transition duration-300 ease-linear group-hover:opacity-20 md:opacity-100 md:group-hover:opacity-100"
             />
           </div>
         </div>
 
         {/* SECOND PROJECT CARD */}
-        <div class="grid grid-cols-10 items-center">
-          <div class="relative z-[-1] col-span-full row-span-full h-full rounded-2xl border-[1px] border-[#797382]/40 p-2 dark:border-[#807C8E]/50 md:col-start-2 md:col-end-6 md:h-fit">
+        <div class="group grid grid-cols-10 items-center">
+          {/* PROJECT PHOTO */}
+          <div class="relative z-[-1] col-span-full row-span-full h-full rounded-2xl border-[1px] border-white p-2 transition duration-300 ease-in-out group-hover:border-2 group-hover:border-[#eaa3c2] dark:border-[#807C8E]/70 dark:group-hover:border-[#7943ED] md:col-start-2 md:col-end-6 md:h-fit">
             <img
               src={watch_soon}
               alt="adopt-a-pal mockups"
-              className="h-full rounded-xl object-cover opacity-20 md:opacity-100"
+              className="h-full w-full rounded-xl object-cover transition duration-300 ease-linear group-hover:opacity-20 md:opacity-100 md:group-hover:opacity-100"
             />
           </div>
-          <div class="col-span-full row-span-full p-3 px-4 text-end md:col-span-5 md:col-start-5 md:px-0">
+
+          {/* PROJECT DESCRIPTION */}
+          <div class="col-span-full row-span-full p-8 px-4 text-end opacity-0 transition duration-300 hover:opacity-100 md:col-span-5 md:col-start-5 md:p-0 md:opacity-100">
             <h4 class="font-mono text-sm">TV and Movie Reminder App</h4>
             <h3 class="mb-2 mt-0 text-2xl font-bold">Watch Soon</h3>
             <div class="">
@@ -102,18 +113,26 @@ const ProjectsSection = () => {
               </li>
             </ul>{" "}
             <div className="mt-3 flex flex-row items-center justify-end space-x-4">
-              <a href="">
+              <a
+                href="https://github.com/encarnac/tv-movie-reminder"
+                className="transition duration-300 ease-in-out hover:scale-110"
+              >
                 <LuGithub className="size-5" />
               </a>
-              <a href="">
+              <a
+                href="https://watch-soon.up.railway.app/"
+                className="transition duration-300 ease-in-out hover:scale-110"
+              >
                 <LuExternalLink className="size-5" />
               </a>
             </div>
           </div>
         </div>
+
         {/* THIRD PROJECT CARD */}
-        <div class="grid grid-cols-10 items-center">
-          <div class="col-span-full row-span-full p-3 px-4 text-start md:col-span-5 md:col-start-2 md:px-0">
+        <div class="group grid grid-cols-10 items-center">
+          {/* PROJECT DESCRIPTION */}
+          <div class="col-span-full row-span-full p-8 px-4 text-start opacity-0 transition duration-300 hover:opacity-100 md:col-span-5 md:col-start-2 md:p-0 md:opacity-100">
             <h4 class="font-mono text-sm">Pet Adoption "Dating" App</h4>
             <h3 class="mb-2 mt-0 text-2xl font-bold">Adopt-a-Pal</h3>
             <div class="">
@@ -137,22 +156,32 @@ const ProjectsSection = () => {
               <li className="rounded-lg border-2 border-[#797382]/40 px-2 py-px dark:border-[#807C8E]/90">
                 GCP
               </li>
+              <li className="rounded-lg border-2 border-[#797382]/40 px-2 py-px dark:border-[#807C8E]/90">
+                Figma
+              </li>
             </ul>
             <div className="mt-3 flex flex-row items-center justify-start space-x-4">
-              <a href="">
+              <a
+                href="https://github.com/encarnac/adopt-a-pal"
+                className="transition duration-300 ease-in-out hover:scale-110"
+              >
                 <LuGithub className="size-5" />
               </a>
-              <a href="">
-                <LuExternalLink className="size-5" />
+              <a
+                href="https://www.figma.com/file/yJkg7EMO0fpvOo6WCn6JMO/Desktop-Prototype?type=design&node-id=0%3A1&mode=design&t=dxbTPTU57jhHoxhv-1"
+                className="transition duration-300 ease-in-out hover:scale-110"
+              >
+                <LuFigma className="size-5" />
               </a>
             </div>
           </div>
 
-          <div class="relative z-[-1] col-span-full row-span-full h-full rounded-2xl border-[1px] border-[#797382]/40 p-2 dark:border-[#807C8E]/50 md:col-start-6 md:col-end-10 md:h-fit">
+          {/* PROJECT PHOTO */}
+          <div class="relative z-[-1] col-span-full row-span-full h-full rounded-2xl border-[1px] border-[#797382]/40 border-white p-2 transition duration-300 ease-in-out group-hover:border-2 group-hover:border-[#eaa3c2] dark:border-[#807C8E]/70 dark:group-hover:border-[#7943ED] md:col-start-6 md:col-end-10 md:h-fit">
             <img
               src={adopt_a_pal}
               alt="adopt-a-pal mockups"
-              className="h-full rounded-xl object-cover opacity-30 md:opacity-100"
+              className="h-full w-full rounded-xl object-cover transition duration-300 ease-linear group-hover:opacity-20 md:opacity-100 md:group-hover:opacity-100"
             />
           </div>
         </div>
