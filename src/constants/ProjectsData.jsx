8750464335e6c34f7f0e582e6595d3dd73153343
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-08 22:25:43 -0800

    Fixed public path to project photos

--- src/constants/ProjectsData.jsx
+++ src/constants/ProjectsData.jsx
@@ -5,7 +5,7 @@ export const PROJECTS_DATA = [
     description:
       "A responsive and interactive website to showcase my background, skills, and projects. Used as a learning tool for using animations and a dark/light mode theme.",
     tags: ["React", "Tailwind", "Framer Motion", "Netlify"],
-    image: "../../public/portfolio.png",
+    image: "/portfolio.png",
     github: "https://github.com/encarnac/portfolio",
     demo: "https://encarnac.netlify.app/",
   },
@@ -15,7 +15,7 @@ export const PROJECTS_DATA = [
     description:
       "A web application for a 'dating' service to connect potential adopters with pets posted by animal shelters. Users can swipe through listings posted by shelters and 'like' pets to create a collection of potential pets.",
     tags: ["React", "Tailwind", "Flask", "GCP", "Figma"],
-    image: "../../public/adopt-a-pal.png",
+    image: "/adopt-a-pal.png",
     github: "https://github.com/encarnac/Adopt-A-Pal",
     demo: "https://www.figma.com/file/yJkg7EMO0fpvOo6WCn6JMO/Desktop-Prototype?type=design&node-id=0%3A1&mode=design&t=TfNgvSewE7efiwJb-1",
   },
@@ -25,7 +25,7 @@ export const PROJECTS_DATA = [
     description:
       "Search for tv shows or movies by title using the The Movie Database API, and add content to your watchlist to receive future release reminders on Google Calendar.",
     tags: ["React", "Bootstrap", "Express", "MongoDB", "GCP", "Railway"],
-    image: "../../public/watch-soon.png",
+    image: "/watch-soon.png",
     github: "https://github.com/encarnac/tv-movie-reminder",
     demo: "https://watch-soon.up.railway.app/",
   },
