14bc098cd7bdba5411916e3ad663f23f5d308fba
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-09 22:59:55 -0800

    Separated skills data in another file and created reusable card component

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,27 +1,8 @@
-import frontend from "../assets/frontend.svg";
-import backend from "../assets/backend.svg";
-import tools from "../assets/tools.svg";
 import { motion } from "framer-motion";
 import {
-  SiHtml5,
-  SiCss3,
-  SiJavascript,
-  SiReact,
-  SiNextdotjs,
-  SiBootstrap,
-  SiTailwindcss,
-  SiPython,
-  SiFlask,
-  SiExpress,
-  SiMongodb,
-  SiMysql,
-  SiGooglecloud,
-  SiGit,
-  SiGithub,
-  SiJirasoftware,
-  SiFigma,
-  SiPostman,
-} from "react-icons/si";
+  SKILLS_DATA_SORTED,
+  SKILLS_DATA_UNSORTED,
+} from "../constants/SkillsData";
 
 const skillCard = {
   hidden: { y: 40, opacity: 0 },
@@ -62,126 +43,34 @@ const SkillsSection = () => {
         variants={skillsContainer}
         className="grid grid-cols-1 justify-items-center gap-x-0 gap-y-4 px-0 sm:px-16 md:grid-cols-12 md:gap-x-4 md:gap-y-0 md:px-0 lg:gap-x-6 xl:gap-x-20 2xl:gap-x-28"
       >
-        <motion.div
-          transition={{ type: "spring", stiffness: 100 }}
-          variants={skillCard}
-          className="relative rounded-3xl backdrop-blur-none before:absolute before:-bottom-[.4rem] before:-left-[.4rem] before:z-[-1] before:h-full before:w-full before:rounded-3xl before:bg-gradient-to-br before:from-[#68A3EB] before:to-[#F38BBB] before:opacity-0 before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:col-span-4"
-        >
-          <div className="flex h-full flex-col items-center justify-start gap-y-5 rounded-3xl bg-[#E3E8EE] px-0 py-10 transition duration-300 hover:-translate-y-2 hover:duration-300 dark:bg-[#231F2F] lg:py-14">
-            <div className="mb-1 rounded-full bg-[#cfd7e5] p-5 dark:bg-[#2b2841]">
-              <img src={frontend} className="w-11" />
+        {SKILLS_DATA_SORTED.map((skill, index) => (
+          <motion.div
+            key={index}
+            transition={{ type: "spring", stiffness: 100 }}
+            variants={skillCard}
+            className="relative rounded-3xl backdrop-blur-none before:absolute before:-bottom-[.4rem] before:-left-[.4rem] before:z-[-1] before:h-full before:w-full before:rounded-3xl before:bg-gradient-to-br before:from-[#68A3EB] before:to-[#F38BBB] before:opacity-0 before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:col-span-4"
+          >
+            <div className="flex h-full flex-col items-center justify-start gap-y-5 rounded-3xl bg-[#E3E8EE] px-0 py-6 transition duration-300 hover:-translate-y-2 hover:duration-300 dark:bg-[#231F2F] lg:py-10">
+              <div className="mb-1 rounded-full bg-[#cfd7e5] p-4 dark:bg-[#2b2841] md:p-5">
+                <img src={skill.image} className="w-8 md:w-11" />
+              </div>
+              <h4 className="relative pb-4 text-base font-bold text-[#5b5662] after:absolute after:inset-x-3 after:bottom-0 after:h-2 after:w-3/4 after:rounded-full after:bg-[#AD99DB] dark:text-white dark:after:bg-[#7943ED] md:text-xl">
+                {skill.category}
+              </h4>
+              <ul className="flex-rows flex flex-wrap justify-center gap-x-1 gap-y-2 px-6 text-sm text-[#6B6573] dark:text-[#CCC3D7] md:text-base">
+                {skill.tools.map((tool, index) => (
+                  <li
+                    key={index}
+                    className="grid grid-cols-3 items-center justify-items-center gap-x-px rounded-xl border-[1px] border-[#6B6573] px-2 py-1 dark:border-[#CCC3D7]"
+                  >
+                    {tool.logo}
+                    <span className="col-span-2">{tool.name}</span>
+                  </li>
+                ))}
+              </ul>
             </div>
-            <h4 className="relative pb-4 text-xl font-bold text-[#5b5662] after:absolute after:inset-x-3 after:bottom-0 after:h-2 after:w-3/4 after:rounded-full after:bg-[#AD99DB] dark:text-white dark:after:bg-[#7943ED]">
-              Front-End
-            </h4>
-            <ul className="flex-rows flex flex-wrap justify-center gap-x-1 gap-y-2 px-8 text-[#6B6573] dark:text-[#CCC3D7]">
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiHtml5 className="col-span-1" />
-                <span className="col-span-2 px-px">HTML5</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiCss3 className="col-span-1" />
-                <span className="col-span-2 px-px">CSS3</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiJavascript className="col-span-1" />
-                <span className="col-span-2 px-px">Javascript</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiReact className="col-span-1" />
-                <span className="col-span-2 px-px">React</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiNextdotjs className="col-span-1" />
-                <span className="col-span-2 px-px">Next.js</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiBootstrap className="col-span-1" />
-                <span className="col-span-2">Bootstrap</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiTailwindcss className="col-span-1" />
-                <span className="col-span-2">Tailwind</span>
-              </li>
-            </ul>
-          </div>
-        </motion.div>
-        <motion.div
-          transition={{ type: "spring", stiffness: 100 }}
-          variants={skillCard}
-          className="relative h-full rounded-3xl backdrop-blur-none before:absolute before:-bottom-[.4rem] before:-left-[.4rem] before:z-[-1] before:h-full before:w-full before:rounded-3xl before:bg-gradient-to-br before:from-[#68A3EB] before:to-[#F38BBB] before:opacity-0 before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:col-span-4"
-        >
-          <div className="flex h-full flex-col items-center justify-start gap-y-5 rounded-3xl bg-[#E3E8EE] px-0 py-10 transition duration-300 hover:-translate-y-2 hover:duration-300 dark:bg-[#231F2F] lg:py-14">
-            <div className="mb-1 rounded-full bg-[#cfd7e5] p-5 dark:bg-[#2b2841]">
-              <img src={backend} className="w-11" />
-            </div>
-            <h4 className="relative pb-4 text-xl font-bold text-[#5b5662] after:absolute after:inset-x-3 after:bottom-0 after:h-2 after:w-3/4 after:rounded-full after:bg-[#AD99DB] dark:text-white dark:after:bg-[#7943ED]">
-              Back-End
-            </h4>
-            <ul className="flex-rows flex flex-wrap justify-center gap-x-1 gap-y-2 px-8 text-[#6B6573] dark:text-[#CCC3D7]">
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiPython className="col-span-1" />
-                <span className="col-span-2 px-px">Python</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiFlask className="col-span-1" />
-                <span className="col-span-2 px-px">Flask</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiExpress className="col-span-1" />
-                <span className="col-span-2 px-px">Express</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiMongodb className="col-span-1" />
-                <span className="col-span-2 px-px">MongoDB</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiMysql className="col-span-1" />
-                <span className="col-span-2 px-px">MySQL</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiGooglecloud className="col-span-1" />
-                <span className="col-span-2 px-px">GCP</span>
-              </li>
-            </ul>
-          </div>
-        </motion.div>
-        <motion.div
-          transition={{ type: "spring", stiffness: 100 }}
-          variants={skillCard}
-          className="relative h-full rounded-3xl backdrop-blur-none before:absolute before:-bottom-[.4rem] before:-left-[.4rem] before:z-[-1] before:h-full before:w-full before:rounded-3xl before:bg-gradient-to-br before:from-[#68A3EB] before:to-[#F38BBB] before:opacity-0 before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:col-span-4"
-        >
-          <div className="flex h-full flex-col items-center justify-start gap-y-5 rounded-3xl bg-[#E3E8EE] px-0 py-10 transition duration-300 hover:-translate-y-2 hover:duration-300 dark:bg-[#231F2F] lg:py-14">
-            <div className="mb-1 rounded-full bg-[#cfd7e5] p-5 dark:bg-[#2b2841]">
-              <img src={tools} className="h-11" />
-            </div>
-            <h4 className="relative pb-4 text-xl font-bold text-[#5b5662] after:absolute after:inset-x-3 after:bottom-0 after:h-2 after:w-3/4 after:rounded-full after:bg-[#AD99DB] dark:text-white dark:after:bg-[#7943ED]">
-              Dev Tools
-            </h4>
-            <ul className="flex-rows flex flex-wrap justify-center gap-x-1 gap-y-2 px-8 text-[#6B6573] dark:text-[#CCC3D7]">
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiGit className="col-span-1" />
-                <span className="col-span-2 px-px">Git</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiGithub className="col-span-1" />
-                <span className="col-span-2 px-px">GitHub</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiJirasoftware className="col-span-1" />
-                <span className="col-span-2 px-px">Jira</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiFigma className="col-span-1" />
-                <span className="col-span-2 px-px">Figma</span>
-              </li>
-              <li className="grid grid-cols-3 items-center justify-items-center rounded-xl border-[1px] border-[#6B6573] px-3 py-1 dark:border-[#CCC3D7]">
-                <SiPostman className="col-span-1" />
-                <span className="col-span-2 px-px">Postman</span>
-              </li>
-            </ul>
-          </div>
-        </motion.div>
+          </motion.div>
+        ))}
       </motion.div>
     </section>
   );
