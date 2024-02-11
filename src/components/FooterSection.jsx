d803538bedec6d46284bc8643edea03b19450523
Colene <coleneencarnado@gmail.com>
2024-02-11 02:20:53 -0800

    Centered content and changed text color

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const FooterSection = () => {
-  return <div>All Rights Reserved</div>;
+  return (
+    <div className="flex flex-row items-center justify-center text-xs text-[#797382] dark:text-white">
+      <p>All Rights Reserved</p>
+    </div>
+  );
 };
 
 export default FooterSection;
