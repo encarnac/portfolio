aa7023f3650b670d86effe528123daab3e2e4dcf
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-13 13:16:55 -0800

    Created 2x2 skills cards on right layout

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -31,9 +31,7 @@ const SkillsSection = () => {
           [02]skills
         </h4>
         <h3 className="font-sans text-3xl font-bold text-[#5b5662] dark:text-[#D4D3D8] lg:text-4xl">
-          {/* <span className="bg-gradient-to-r from-[#68A3EB] to-[#F38BBB] bg-clip-text text-transparent dark:from-[#3034C2] dark:via-[#6A3DD1] dark:to-[#9329BE]"> */}
           What I Do
-          {/* </span> */}
         </h3>
       </div>
       <motion.div
@@ -41,38 +39,30 @@ const SkillsSection = () => {
         whileInView="visible"
         viewport={{ once: true, amount: 0.4 }}
         variants={skillsContainer}
-        className="grid grid-cols-1 justify-items-center gap-3 sm:grid-cols-2 md:grid-cols-4 lg:gap-4 xl:gap-8 2xl:gap-14"
+        y
+        className="grid grid-cols-12"
       >
-        {SKILLS_DATA_SORTED.map((skill, index) => (
-          <motion.div
-            key={index}
-            transition={{ type: "spring", stiffness: 50 }}
-            variants={skillCard}
-            className="relative h-full w-full backdrop-blur-none before:absolute before:-bottom-[.3rem] before:-left-[.3rem] before:z-[-1] before:h-full before:w-full before:rounded-xl before:bg-gradient-to-br before:from-[#68A3EB] before:to-[#F38BBB] before:opacity-0 before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:before:rounded-2xl"
-          >
-            <div className="flex h-full flex-col items-center justify-start gap-y-2 rounded-xl bg-[#E3E8EE] px-0 py-5 transition duration-300 hover:-translate-y-2 hover:duration-300 dark:bg-[#211e2c] md:rounded-2xl xl:py-8">
-              <div className="mb-px rounded-full bg-[#cfd7e5] p-4 dark:bg-[#2b2841] lg:p-5">
-                <img src={skill.image} className="w-7 md:w-9" />
+        <div className="col-span-6"></div>
+        <div className="col-span-5 col-end-12 grid grid-cols-2 items-start justify-items-start gap-2">
+          {SKILLS_DATA_SORTED.map((skill, index) => (
+            <motion.div
+              key={index}
+              transition={{ type: "spring", stiffness: 50 }}
+              variants={skillCard}
+              className="relative h-full w-full backdrop-blur-none before:absolute before:-bottom-[.3rem] before:-left-[.3rem] before:z-[-1] before:h-full before:w-full before:rounded-xl before:bg-gradient-to-br before:from-[#68A3EB] before:to-[#F38BBB] before:opacity-0 before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:before:rounded-2xl"
+            >
+              <div className="flex h-full flex-col items-center justify-start gap-y-2 rounded-xl bg-[#E3E8EE] px-0 py-5 transition duration-300 hover:-translate-y-2 hover:duration-300 dark:bg-[#211e2c] md:rounded-2xl xl:py-8">
+                <div className="mb-px rounded-full bg-[#cfd7e5] p-4 dark:bg-[#2b2841] lg:p-5">
+                  <img src={skill.image} className="w-7 md:w-9" />
+                </div>
+                <h4 className="relative text-base font-bold text-[#5b5662] dark:text-[#D4D3D8] sm:text-sm md:text-base lg:text-lg">
+                  {skill.category}
+                </h4>
+                <span className="h-[.3rem] w-10 rounded-sm bg-[#AD99DB] dark:bg-[#7943ED] md:mt-0"></span>
               </div>
-              <h4 className="relative text-base font-bold text-[#5b5662] dark:text-[#D4D3D8] sm:text-sm md:text-base lg:text-lg">
-                {skill.category}
-              </h4>
-              <span className="h-[.3rem] w-10 rounded-sm bg-[#AD99DB] dark:bg-[#7943ED] md:mt-0"></span>
-
-              <ul className="2 grid grid-cols-4 justify-center gap-1 gap-y-1 px-1 text-xs text-[#7B7484] dark:text-[#c3c3c4] sm:grid-cols-2 sm:text-sm lg:text-base">
-                {skill.tools.map((tool, index) => (
-                  <li
-                    key={index}
-                    className="flex flex-col items-center justify-items-center gap-y-1 px-0 py-1"
-                  >
-                    <span className="">{tool.name}</span>
-                    {tool.logo}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </motion.div>
-        ))}
+            </motion.div>
+          ))}
+        </div>
       </motion.div>
     </section>
   );
