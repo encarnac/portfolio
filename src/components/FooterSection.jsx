dcac68faca92df623392349a1ea215a445b4a12f
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-13 12:20:27 -0800

    Footer text no wrap

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const FooterSection = () => {
   return (
     <div className="flex flex-col items-center justify-center font-mono text-xs text-[#7B7484] dark:text-[#c3c3c4]">
-      <p> Designed & Built by Colene Encarnado </p>
+      <p className="text-nowrap"> Designed & Built by Colene Encarnado </p>
       <p>© 2024. All Rights Reserved</p>
     </div>
   );
