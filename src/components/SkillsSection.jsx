57e795b126684ada78462f87ec506be20085536a
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-13 18:11:50 -0800

    Progress bars on left switch depending on selected tab

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,8 +1,7 @@
-import { motion } from "framer-motion";
-import {
-  SKILLS_DATA_SORTED,
-  SKILLS_DATA_UNSORTED,
-} from "../constants/SkillsData";
+import { useState, useTransition } from "react";
+
+import { motion, AnimatePresence } from "framer-motion";
+import { SKILLS_DATA } from "../constants/SkillsData";
 
 const skillCard = {
   hidden: { y: 40, opacity: 0 },
@@ -24,6 +23,15 @@ const skillsContainer = {
 };
 
 const SkillsSection = () => {
+  const [tab, setTab] = useState("Languages");
+  const [isPending, startTransition] = useTransition();
+
+  const selectTab = (nextTab) => {
+    startTransition(() => {
+      setTab(nextTab);
+    });
+  };
+
   return (
     <section id="skills" className="mb-12 w-full">
       <div className="relative mb-8 flex flex-col items-start justify-center py-3">
@@ -40,25 +48,71 @@ const SkillsSection = () => {
         viewport={{ once: true, amount: 0.4 }}
         variants={skillsContainer}
         y
-        className="grid grid-cols-2 md:grid-cols-12"
+        className="grid grid-cols-2 items-center md:grid-cols-12"
       >
-        <div className="col-span-full"></div>
-        <div className="col-span-full grid grid-cols-2 items-start justify-items-start gap-2 md:col-span-5 md:col-end-12">
-          {SKILLS_DATA_SORTED.map((skill, index) => (
+        {/* SKILLS CONTENT */}
+        <div className="col-span-6">
+          {" "}
+          {/* <AnimatePresence mode="wait"> */}
+          <div
+            // key={tab}
+            // initial={{ y: -25, opacity: 0 }}
+            // animate={{ y: 0, opacity: 1 }}
+            // exit={{ y: 25, opacity: 0 }}
+            // transition={{ duration: 0.2 }}
+            className="space-y-3 py-4 text-[#5b5662] dark:text-[#D4D3D8] sm:px-12"
+          >
+            {SKILLS_DATA.find((t) => t.category === tab).tools.map(
+              (skill, index) => (
+                <div className="space-y-2">
+                  <div className="flex flex-row justify-between">
+                    <p>{skill.name}</p>
+                    <p>{skill.percentage}</p>
+                  </div>
+                  <div className="h-3 w-full rounded-xl bg-[#CED7E5] dark:bg-[#2B2841]">
+                    <div
+                      className={`h-3 w-[${skill.percentage}] rounded-xl bg-gradient-to-r transition-all duration-300 ease-in-out from-[#A09AD8] to-[#CD94CA] dark:from-[#4E39CA] dark:to-[#703BCF]`}
+                    ></div>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+          {/* </AnimatePresence> */}
+        </div>
+
+        {/* SKILLS TABS */}
+        <div className="col-span-full mt-6 grid grid-cols-2 items-start justify-items-start gap-x-3 gap-y-4 sm:mt-0 md:col-span-5 md:col-end-12">
+          {SKILLS_DATA.map((skill, index) => (
             <motion.div
+              onClick={() => selectTab(skill.category)}
               key={index}
               transition={{ type: "spring", stiffness: 50 }}
               variants={skillCard}
-              className="relative h-full w-full backdrop-blur-none before:absolute before:-bottom-[.3rem] before:-left-[.3rem] before:z-[-1] before:h-full before:w-full before:rounded-xl before:bg-gradient-to-br before:from-[#68A3EB] before:to-[#F38BBB] before:opacity-0 before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:before:rounded-2xl"
+              className={`${
+                tab === skill.category
+                  ? "before:opacity-100 before:-translate-y-2 "
+                  : "before:opacity-0"
+              } relative cursor-pointer h-full w-full backdrop-blur-none before:absolute before:-bottom-[.3rem] before:-left-[.3rem] before:z-[-1] before:h-full before:w-full before:rounded-xl before:bg-gradient-to-br before:from-[#68A3EB] before:to-[#F38BBB] before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:before:rounded-2xl`}
             >
-              <div className="flex h-full flex-col items-center justify-start gap-y-1 rounded-xl bg-[#E3E8EE] px-0 py-3 transition duration-300 hover:-translate-y-2 hover:duration-300 dark:bg-[#211e2c] md:gap-y-2 md:rounded-2xl xl:py-8">
+              <div
+                className={`${
+                  tab === skill.category ? "-translate-y-2 " : ""
+                } flex h-full flex-col items-center justify-start gap-y-1 rounded-xl bg-[#E3E8EE] px-0 py-3 transition duration-300 hover:-translate-y-2 hover:duration-300 dark:bg-[#211e2c] md:gap-y-2 md:rounded-2xl xl:py-8`}
+              >
                 <div className="mb-px rounded-full bg-[#cfd7e5] p-3 dark:bg-[#2b2841] lg:p-5">
                   <img src={skill.image} className="w-5 md:w-9" />
                 </div>
                 <h4 className="relative text-base font-bold text-[#5b5662] dark:text-[#D4D3D8] sm:text-sm md:text-base lg:text-lg">
                   {skill.category}
                 </h4>
-                <span className="h-[.3rem] w-10 rounded-sm bg-[#AD99DB] dark:bg-[#7943ED] md:mt-0"></span>
+                <span
+                  className={`${
+                    tab === skill.category
+                      ? "bg-[#AD99DB] dark:bg-[#7943ED]"
+                      : "bg-[#AD99DB]/30 dark:bg-[#7943ED]/20"
+                  } h-[.3rem] transition duration-100 ease-in w-10 rounded-sm md:mt-0`}
+                ></span>
               </div>
             </motion.div>
           ))}
