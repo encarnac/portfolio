8c3cb31aa719943a170313b987aa5c68d08711d4
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-14 21:21:53 -0800

    Changed percentage number font and changed progress bar background color

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,14 +1,14 @@
 const SkillCard = ({ skills }) => {
   return (
-    <div className="space-y-2">
+    <div className="space-y-3">
       {skills.map((skill, index) => (
         <div key={`skill_${index}`}>
           <div className="flex flex-row justify-between">
             <p>{skill.skill}</p>
-            <p>{skill.percentage}</p>
+            <p className="font-mono">{skill.percentage}</p>
           </div>
 
-          <div className="h-4 w-full rounded-xl bg-[#E3E8EE] dark:bg-[#2B2841]">
+          <div className="h-4 w-full rounded-xl bg-[#E3E8EE]/70 dark:bg-[#201E2C]/70">
             <div
               style={{ width: skill.percentage }}
               className={`h-4 rounded-xl bg-gradient-to-r from-[#A09AD8] to-[#CD94CA] transition-all ease-in-out dark:from-[#4E39CA] dark:to-[#703BCF]`}
