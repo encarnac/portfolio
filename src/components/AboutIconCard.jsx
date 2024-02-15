98eaf0191c2e61336a3044d8cdc68357c900c893
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-14 21:16:41 -0800

    Changed dark mode color and moved color styling to component for readability

--- src/components/AboutIconCard.jsx
+++ src/components/AboutIconCard.jsx
@@ -1,9 +1,11 @@
 const AboutIconCard = ({ info }) => {
   return (
-    <div className="flex flex-col items-center gap-x-1 gap-y-2 rounded-lg px-1 py-2 font-semibold backdrop-blur-lg sm:gap-x-2 sm:px-2 md:flex-row md:justify-center md:gap-y-0">
+    <div className="flex flex-col items-center gap-x-1 gap-y-2 rounded-lg px-1 py-2 font-semibold text-[#D6C5DC] backdrop-blur-lg dark:text-[#2B2841] sm:gap-x-2 sm:px-2 md:flex-row md:justify-center md:gap-y-0">
       {info.icon}
       <div className="flex flex-col">
-        <p className="leading-none">{info.firstLine}</p>
+        <p className="leading-none text-[#5b5662] dark:text-[#D4D3D8]">
+          {info.firstLine}
+        </p>
         <p className="text-sm font-normal text-[#ADADB6] dark:text-[#686671] md:text-xs lg:text-base">
           {info.secondLine}
         </p>
