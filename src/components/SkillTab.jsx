bd92fb205141ccf95d664eb7b06ca217ee17f520
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-14 21:26:32 -0800

    Centered content along column and changed light mode underline to pink

--- src/components/SkillTab.jsx
+++ src/components/SkillTab.jsx
@@ -19,12 +19,12 @@ const SkillTab = ({ index, skill, tab, selectTab }) => {
         tab === skill
           ? "before:opacity-100 before:-translate-y-2 "
           : "before:opacity-0"
-      } relative cursor-pointer h-full w-full backdrop-blur-none before:absolute before:-bottom-[.3rem] before:-left-[.3rem] before:z-[-1] before:h-full before:w-full before:rounded-xl before:bg-gradient-to-br before:from-[#68A3EB] before:to-[#F38BBB] before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:before:rounded-2xl`}
+      } relative cursor-pointer h-full w-full backdrop-blur-none before:absolute before:-bottom-[.3rem] before:-left-[.3rem] before:z-[-1] before:h-full before:w-full before:rounded-xl before:bg-gradient-to-br  before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:from-[#68A3EB] before:to-[#F38BBB] before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:before:rounded-2xl`}
     >
       <div
         className={`${
           tab === skill ? "-translate-y-2 " : ""
-        } flex h-full flex-col items-center justify-start gap-y-1 rounded-xl bg-[#E3E8EE] px-0 py-3 transition duration-300 hover:-translate-y-2 hover:duration-300 dark:bg-[#211e2c] md:gap-y-2 md:rounded-2xl xl:py-8`}
+        } flex h-full flex-col justify-center items-center gap-y-1 rounded-xl bg-[#E3E8EE] px-0 py-3 transition duration-300 hover:-translate-y-2 hover:duration-300 dark:bg-[#211e2c] md:gap-y-2 md:rounded-2xl xl:py-8`}
       >
         <div className="mb-px rounded-full bg-[#cfd7e5] p-3 dark:bg-[#2b2841] lg:p-5">
           <img src={skill.image} className="w-5 md:w-9" />
@@ -37,8 +37,8 @@ const SkillTab = ({ index, skill, tab, selectTab }) => {
         <spans
           className={`${
             tab === skill
-              ? "bg-[#AD99DB] dark:bg-[#7943ED]"
-              : "bg-[#AD99DB]/30 dark:bg-[#7943ED]/20"
+              ? "bg-[#DE8ABB] dark:bg-[#7943ED]"
+              : "bg-[#DE8ABB]/30 dark:bg-[#7943ED]/20"
           } h-[.3rem] transition duration-100 ease-in w-10 rounded-sm md:mt-0`}
         ></spans>
       </div>
