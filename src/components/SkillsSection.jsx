1975f46c00467b2dad8e517a10b15e86d5f4c766
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-14 21:23:33 -0800

    Moved header to left grid column and breakpoints for grid span

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -24,41 +24,43 @@ const SkillsSection = () => {
   };
 
   return (
-    <section id="skills" className="mb-12 w-full">
-      {/* SECTION HEADER */}
-      <div className="relative mb-8 flex flex-col items-start justify-center py-3">
-        <h4 className="l font-mono text-lg tracking-widest text-[#5b5662]/40 dark:text-[#D4D3D8]/40 lg:text-xl">
-          [02]skills
-        </h4>
-        <h3 className="font-sans text-3xl font-bold text-[#5b5662] dark:text-[#D4D3D8] lg:text-4xl">
-          What I Do
-        </h3>
-      </div>
-
+    <section id="skills" className="my-20 w-full">
       {/* SECTION CONTENT */}
       <motion.div
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true, amount: 0.4 }}
         variants={skillsContainer}
-        y
-        className="grid grid-cols-2 items-center md:grid-cols-12"
+        className="grid grid-cols-2 md:grid-cols-11 md:gap-x-4"
       >
-        {/* SKILLS CONTENT */}
-        <div className="col-span-full md:col-span-4 md:col-end-6">
+        <div className="col-span-full flex flex-col justify-center md:col-span-5 md:col-end-6 xl:col-span-4 xl:col-end-6">
+          <div className="place-self-start md:place-self-end">
+            {/* SECTION HEADER */}
+            <h4 className="text-start font-mono text-lg tracking-widest text-[#5b5662]/40 dark:text-[#D4D3D8]/40 md:text-end lg:text-xl">
+              skills
+            </h4>
+            <h3 className="font-sans text-3xl font-bold text-[#5b5662] dark:text-[#D4D3D8] lg:text-4xl">
+              What I{" "}
+              <span className="bg-gradient-to-r from-[#68A3EB] to-[#F38BBB] bg-clip-text text-transparent dark:from-[#3034C2] dark:via-[#6A3DD1] dark:to-[#9329BE]">
+                Do
+              </span>
+            </h3>
+          </div>
+
+          {/* SKILLS CONTENT */}
           <motion.div
             key={tab}
             initial={{ y: -25, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.6 }}
-            className="space-y-4 py-4 text-[#5b5662] dark:text-[#D4D3D8]"
+            className="py-4 text-[#5b5662] dark:text-[#D4D3D8]"
           >
             <SkillCard skills={tab.skills} />
           </motion.div>
         </div>
 
         {/* SKILLS TABS */}
-        <div className="col-span-full mt-6 grid grid-cols-2 items-start justify-items-start gap-x-3 gap-y-4 sm:mt-0 md:col-span-5 md:col-end-12">
+        <div className="col-span-full mt-6 grid h-full grid-cols-2 items-start justify-items-start gap-x-3 gap-y-4 md:col-span-5 md:col-end-12 md:mt-0 xl:col-span-4 xl:col-end-11">
           {SKILLS_DATA.map((skill, index) => (
             <SkillTab
               key={index}
