937e618e4f0bedbac661762de1ee90b32e7c6a3a
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-16 19:31:09 -0800

    Added id and project type

--- src/utils/ProjectsData.jsx
+++ src/utils/ProjectsData.jsx
@@ -1,50 +1,60 @@
 export const PROJECTS_DATA = [
   {
+    id: 1,
     title: "Portfolio",
     subtitle: "React Portfolio Website",
     description:
       "A responsive and interactive website to showcase my background, skills, and projects. Used as a learning tool for using animations and a dark/light mode theme.",
     tags: ["React", "Tailwind", "Framer Motion", "Netlify"],
+    type: ["all", "web"],
     image: "/portfolio.png",
     github: "https://github.com/encarnac/portfolio",
     demo: "https://encarnac.netlify.app/",
   },
   {
+    id: 2,
     title: "Adopt-a-Pal",
     subtitle: "Pet Adoption App",
     description:
       "A web application for a 'dating' service to connect potential adopters with pets posted by animal shelters. Users can swipe through listings posted by shelters and 'like' pets to create a collection of potential pets.",
     tags: ["React", "Tailwind", "Flask", "GCP", "Figma"],
+    type: ["all", "web"],
     image: "/adopt-a-pal.png",
     github: "https://github.com/encarnac/Adopt-A-Pal",
     demo: "https://www.figma.com/file/yJkg7EMO0fpvOo6WCn6JMO/Desktop-Prototype?type=design&node-id=0%3A1&mode=design&t=TfNgvSewE7efiwJb-1",
   },
   {
+    id: 3,
     title: "Watch Soon",
     subtitle: "TV & Movie Reminder App",
     description:
       "Search for tv shows or movies by title using the The Movie Database API, and add content to your watchlist to receive future release reminders on Google Calendar.",
     tags: ["React", "Bootstrap", "Express", "MongoDB", "GCP", "Railway"],
+    type: ["all", "web"],
     image: "/watch-soon.png",
     github: "https://github.com/encarnac/tv-movie-reminder",
     demo: "https://watch-soon.up.railway.app/",
   },
   {
+    id: 4,
     title: "Papermate",
     subtitle: "MySQL Database Manager",
     description:
       "A web application for administrators to manage a MySQL database for a book subscription service via CRUD operations.",
     tags: ["React", "Bootstrap", "Express", "MySQL"],
+    type: ["all", "web"],
     image: "/papermate.png",
     github: "https://github.com/encarnac/tv-movie-reminder",
     demo: "/",
   },
   {
+    id: 5,
     title: "Readr",
     subtitle: "Reading App Design",
     description:
       "An interactive prototype for a mobile app used to track reading habits and discover new books.",
     tags: ["Figma"],
+    type: ["all", "design"],
     image: "/readr.png",
     github:
       "https://www.figma.com/file/aGwVuq0U4WMDPgyps76Oai/Readr?type=design&node-id=0-1&mode=design&t=Y7jCxqU69T2SPtHt-0",
@@ -52,21 +62,25 @@ export const PROJECTS_DATA = [
   },
 
   {
+    id: 6,
     title: "Mood Journal",
     subtitle: "Journal Mobile App",
     description:
-      "A cross-platform mobile app for journaling and recording your daily mood. Includes a dark mode and landscape view.",
+      "A cross-platform mobile app for journaling and recording your daily mood.",
     tags: ["Flutter", "SQLite"],
+    type: ["all", "mobile"],
     image: "/journal.png",
     github: "https://github.com/encarnac/mood-journal",
     demo: "/",
   },
   {
+    id: 7,
     title: "Wasteagram",
     subtitle: "Food Waste Tracker",
     description:
       "A cross-platform mobile app that enables coffee shop employees to document daily food waste in the form of 'posts' consisting of a photo, number of leftover items, the current date, and the location of the device when the post is created.",
     tags: ["Flutter", "Firebase", "Sentry"],
+    type: ["all", "mobile"],
     image: "/wasteagram.png",
     github: "https://github.com/encarnac/wasteagram",
     demo: "/",
