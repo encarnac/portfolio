b34f7cdb5d5d376a08112fe59d64a980c34b5d8e
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-17 02:12:38 -0800

    Reordered, smaller description, expanded tags

--- src/utils/ProjectsData.jsx
+++ src/utils/ProjectsData.jsx
@@ -2,9 +2,9 @@ export const PROJECTS_DATA = [
   {
     id: 1,
     title: "Portfolio",
-    subtitle: "React Portfolio Website",
+    subtitle: "Personal Website",
     description:
-      "A responsive and interactive website to showcase my background, skills, and projects. Used as a learning tool for using animations and a dark/light mode theme.",
+      "A responsive and interactive website to showcase my background, skills, and projects.",
     tags: ["React", "Tailwind", "Framer Motion", "Netlify"],
     type: ["all", "web"],
     image: "/portfolio.png",
@@ -16,8 +16,8 @@ export const PROJECTS_DATA = [
     title: "Adopt-a-Pal",
     subtitle: "Pet Adoption App",
     description:
-      "A web application for a 'dating' service to connect potential adopters with pets posted by animal shelters. Users can swipe through listings posted by shelters and 'like' pets to create a collection of potential pets.",
-    tags: ["React", "Tailwind", "Flask", "GCP", "Figma"],
+      "A pet adoption 'matching' service to connect potential adopters with pets looking for a home.",
+    tags: ["React", "Tailwind", "Flask", "Datastore", "Cloud Storage", "Figma"],
     type: ["all", "web"],
     image: "/adopt-a-pal.png",
     repo: "https://github.com/encarnac/Adopt-A-Pal",
@@ -26,10 +26,17 @@ export const PROJECTS_DATA = [
   {
     id: 3,
     title: "Watch Soon",
-    subtitle: "TV & Movie Reminder App",
+    subtitle: "TV/Movie Reminder App",
     description:
-      "Search for tv shows or movies by title using the The Movie Database API, and add content to your watchlist to receive future release reminders on Google Calendar.",
-    tags: ["React", "Bootstrap", "Express", "MongoDB", "GCP", "Railway"],
+      "Create a watchlist and receive future release reminders on your Google Calendar.",
+    tags: [
+      "React",
+      "Bootstrap",
+      "Express",
+      "MongoDB",
+      "Google Calander",
+      "Railway",
+    ],
     type: ["all", "web"],
     image: "/watch-soon.png",
     repo: "https://github.com/encarnac/tv-movie-reminder",
@@ -37,27 +44,34 @@ export const PROJECTS_DATA = [
   },
   {
     id: 4,
-    title: "Papermate",
-    subtitle: "MySQL Database Manager",
+    title: "Wasteagram",
+    subtitle: "Waste Tracker App",
     description:
-      "A web application for administrators to manage a MySQL database for a book subscription service via CRUD operations.",
+      "Restaurants can document daily food waste by taking photos and creating posts to track total waste.",
+    tags: [
+      "Dart",
+      "Flutter",
+      "Firebase",
+      "Firestore",
+      "Cloud Storage",
+      "Sentry",
+    ],
+    type: ["all", "mobile"],
+    image: "/wasteagram.png",
+    repo: "https://github.com/encarnac/wasteagram",
+    demo: null,
+  },
+  {
+    id: 5,
+    title: "Papermate",
+    subtitle: "Database Manager",
+    description:
+      "A MySQL database manager for administrators of a book subscription service.",
     tags: ["React", "Bootstrap", "Express", "MySQL"],
     type: ["all", "web"],
     image: "/papermate.png",
     repo: "https://github.com/encarnac/papermate",
     demo: null,
-  },
-  {
-    id: 5,
-    title: "Readr",
-    subtitle: "Reading App Design",
-    description:
-      "An interactive prototype for a mobile app used to track reading habits and discover new books.",
-    tags: ["Figma"],
-    type: ["all", "design"],
-    image: "/readr.png",
-    repo: "https://www.figma.com/file/aGwVuq0U4WMDPgyps76Oai/Readr?type=design&node-id=0-1&mode=design&t=Y7jCxqU69T2SPtHt-0",
-    demo: "https://www.figma.com/proto/aGwVuq0U4WMDPgyps76Oai/Readr?type=design&node-id=0-1&t=FeOLp6hLGMlAlKzU-0&scaling=scale-down&page-id=0%3A1&starting-point-node-id=14%3A645",
   },
 
   {
@@ -65,8 +79,8 @@ export const PROJECTS_DATA = [
     title: "Mood Journal",
     subtitle: "Journal Mobile App",
     description:
-      "A cross-platform mobile app for journaling and recording your daily mood.",
-    tags: ["Flutter", "SQLite"],
+      "A cross-platform mobile app for writing journal posts and recording your daily mood.",
+    tags: ["Dart", "Flutter", "SQLite"],
     type: ["all", "mobile"],
     image: "/journal.png",
     repo: "https://github.com/encarnac/mood-journal",
@@ -74,14 +88,14 @@ export const PROJECTS_DATA = [
   },
   {
     id: 7,
-    title: "Wasteagram",
-    subtitle: "Food Waste Tracker",
+    title: "Readr",
+    subtitle: "Reading App Design",
     description:
-      "A cross-platform mobile app that enables coffee shop employees to document daily food waste in the form of 'posts' consisting of a photo, number of leftover items, the current date, and the location of the device when the post is created.",
-    tags: ["Flutter", "Firebase", "Sentry"],
-    type: ["all", "mobile"],
-    image: "/wasteagram.png",
-    repo: "https://github.com/encarnac/wasteagram",
-    demo: null,
+      "An interactive prototype for a reading habit tracker and book discovery mobile app.",
+    tags: ["Figma"],
+    type: ["all", "design"],
+    image: "/readr.png",
+    repo: "https://www.figma.com/file/aGwVuq0U4WMDPgyps76Oai/Readr?type=design&node-id=0-1&mode=design&t=Y7jCxqU69T2SPtHt-0",
+    demo: "https://www.figma.com/proto/aGwVuq0U4WMDPgyps76Oai/Readr?type=design&node-id=0-1&t=FeOLp6hLGMlAlKzU-0&scaling=scale-down&page-id=0%3A1&starting-point-node-id=14%3A645",
   },
 ];
