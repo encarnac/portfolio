f53116eccb198302788848d1e817890bb7bb64d6
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-16 19:31:43 -0800

    Added entrance animation to each card and changed divs to ul/li

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,43 +1,38 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
-import ProjectCard from "./ProjectCard";
+import { useState, useRef } from "react";
+import { motion, useInView } from "framer-motion";
 import ProjectsTab from "./ProjectsTab";
+import ProjectCard from "./ProjectCard";
 import { PROJECTS_DATA } from "../utils/ProjectsData";
-
-const projectsContainer = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      delayChildren: 0.5,
-      staggerChildren: 0.4,
-    },
-  },
-};
-
-const projectsTabs = {
-  hidden: { y: -30, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
-    transition: { duration: 0.5 },
-  },
-};
-
-const projectCard = {
-  hidden: { y: 30, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
-  },
-};
-
-const types = ["all", "web", "mobile", "design"];
 
 const ProjectsSection = () => {
   const [filter, setFilter] = useState("all");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   const selectFilter = (type) => {
     setFilter(type);
+  };
+
+  const filteredProjects = PROJECTS_DATA.filter((project) =>
+    project.type.includes(filter)
+  );
+
+  const types = ["all", "web", "mobile", "design"];
+
+  const tabVariants = {
+    hidden: { y: -30, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
+  const cardVariants = {
+    hidden: { y: 30, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
   };
 
   return (
@@ -57,7 +52,7 @@ const ProjectsSection = () => {
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 1 }}
-          variants={projectsTabs}
+          variants={tabVariants}
           transition={{ type: "tween", duration: 0.3 }}
           className="relative mt-8 flex h-fit w-fit flex-row place-self-center border-b-[2px] border-b-[#726C7B]/20 px-2 dark:border-b-[#b7b0c2]/10 sm:px-6 lg:col-span-7 lg:mx-0"
         >
@@ -76,17 +71,23 @@ const ProjectsSection = () => {
 
       <div className="flex flex-col items-center gap-y-12">
         {/* PROJECT CARDS */}
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.1 }}
-          variants={projectsContainer}
-          className="grid gap-y-8 text-[#312f34] dark:text-[#c3c3c4] md:grid-cols-12 md:gap-x-2 md:gap-y-8 lg:gap-x-8 xl:gap-x-12"
+        <ul
+          ref={ref}
+          className="grid gap-y-8 text-[#312f34] dark:text-[#c3c3c4] sm:grid-cols-12 sm:gap-x-4 md:gap-x-3 lg:gap-x-8 xl:gap-x-12"
         >
-          {PROJECTS_DATA.map((project, index) => (
-            <ProjectCard key={index} project={project} />
+          {filteredProjects.map((project, index) => (
+            <motion.li
+              key={index}
+              variants={cardVariants}
+              initial="hidden"
+              animate={isInView ? "visible" : "hidden"}
+              transition={{ duration: 0.4, delay: index * 0.5 }}
+              className="col-span-1 flex flex-col items-center sm:col-span-6 md:col-span-4"
+            >
+              <ProjectCard key={project.id} project={project} />
+            </motion.li>
           ))}
-        </motion.div>
+        </ul>
 
         {/* GITHUB LINK */}
         <div className="place-self-center text-white">
