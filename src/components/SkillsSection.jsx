5ca54f1257b70a6905e3b261ec977b374af3079e
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-19 15:58:55 -0800

    Work-around so parent component animates width

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -26,9 +26,11 @@ const SkillsSection = () => {
 
   const cardsVariants = {
     hidden: {
+      width: "0%",
       opacity: 0,
     },
     visible: {
+      width: "100%",
       opacity: 1,
     },
   };
