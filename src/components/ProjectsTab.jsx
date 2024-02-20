6c4d222bd9b5e01c5b83c5c9f0e6d3f686f10f74
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-20 11:41:52 -0800

    Added hover animation with CSS

--- src/components/ProjectsTab.jsx
+++ src/components/ProjectsTab.jsx
@@ -9,16 +9,17 @@ const ProjectsTab = ({ active, selectFilter, type }) => {
         active === type
           ? "text-[#5B5662] dark:text-[#c3c3c4]"
           : "dark:text-[#c3c3c4]/60 text-[#5B5662]/70 hover:text-[#5B5662] dark:hover:text-[#c3c3c4]"
-      } transition duration-300 px-1 md:px-4 relative text-base lg:text-lg font-semibold py-2`}
+      } group transition duration-300 px-1 md:px-5 relative text-base lg:text-lg font-semibold py-2  `}
     >
       <span className="relative">{type} </span>
+      <div className="after:absolute after:-bottom-[.22rem] after:left-0 after:h-[.3rem] after:w-0 after:rounded-sm after:bg-[#282728] after:opacity-0 after:transition-all after:duration-500 group-hover:after:w-full group-hover:after:opacity-100 after:dark:bg-[#7943ed]"></div>
 
       {active === type && (
         <motion.div
-          layout
-          className="absolute -bottom-[.22rem] left-0 h-[.3rem] w-fit rounded-sm bg-[#de8abb] dark:bg-[#7943ed]"
-          animate={{ width: "100%", transition: { duration: 0.4 } }}
-        />
+          initial={{ width: "25%" }}
+          className="absolute -bottom-[.22rem] left-0 h-[.3rem] w-full rounded-sm bg-[#de8abb] dark:bg-[#7943ed]"
+          animate={{ width: "100%", transition: { duration: 0.5 } }}
+        ></motion.div>
       )}
     </button>
   );
