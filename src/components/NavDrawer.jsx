48b511841bb272b195399c2a798f75c00b379cf4
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-21 14:56:37 -0800

    Nav links wait for drawer to expand

--- src/components/NavDrawer.jsx
+++ src/components/NavDrawer.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "./NavLink";
 
 export const NavDrawer = ({ pages, selectedPage, setSelectedPage }) => {
   return (
-    <ul className="flex flex-col items-center py-4 text-[#5b5662] dark:text-[#b7b0c2]">
+    <ul className="flex h-full flex-col items-center py-4 text-[#5b5662] dark:text-[#b7b0c2]">
       {pages.map((page, index) => (
         <li className={index}>
           <NavLink
