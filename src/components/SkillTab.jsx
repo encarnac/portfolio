62a275489cf4acc2c97a3515da7167aa1f67f771
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-21 14:57:46 -0800

    Fixed background and card foreground hovering separately

--- src/components/SkillTab.jsx
+++ src/components/SkillTab.jsx
@@ -18,14 +18,14 @@ const SkillTab = ({ index, skill, tab, selectTab }) => {
         tab === skill
           ? "before:opacity-100 before:-translate-y-2 "
           : "before:opacity-0"
-      } relative cursor-pointer h-full w-full backdrop-blur-none before:absolute before:-bottom-[.3rem] before:-left-[.3rem] before:z-[-1] before:h-full before:w-full before:rounded-xl before:bg-gradient-to-br  before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:from-[#68A3EB] before:to-[#F38BBB] before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:before:rounded-2xl`}
+      } group relative cursor-pointer h-full w-full backdrop-blur-none before:absolute before:-bottom-[.3rem] before:-left-[.3rem] before:z-[-1] before:h-full before:w-full before:rounded-xl before:bg-gradient-to-br  before:transition before:duration-300 hover:before:-translate-y-2 before:hover:opacity-100 before:hover:duration-300 before:from-[#68A3EB] before:to-[#F38BBB] before:dark:from-[#3034C2] before:dark:via-[#6A3DD1] before:dark:to-[#9329BE] md:before:rounded-2xl`}
     >
       <div
         className={`${
           tab === skill ? "-translate-y-2 " : ""
-        } flex h-full flex-col justify-center items-center gap-y-1 rounded-xl bg-[#E3E8EE] px-0 py-3 transition duration-300 hover:-translate-y-2 hover:duration-300 dark:bg-[#211e2c] md:gap-y-2 md:rounded-2xl xl:py-8`}
+        } flex h-full flex-col justify-center items-center gap-y-1 rounded-xl bg-[#E3E8EE] px-0 py-3 transition duration-300 group-hover:-translate-y-2 group-hover:duration-300 dark:bg-[#211e2c] md:gap-y-2 md:rounded-2xl xl:py-8`}
       >
-        <div className="mb-px rounded-full bg-[#cfd7e5] p-3 dark:bg-[#2b2841] lg:p-5">
+        <div className="mb-px rounded-full bg-[#cfd7e5] p-3 transition-colors dark:bg-[#2b2841] lg:p-5">
           <img src={skill.image} className="w-5 md:w-9" />
         </div>
 
