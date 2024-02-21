13d4daeb83287f3885c0fa2eed7170f538c660bb
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-20 18:54:14 -0800

    Removed unnecessary styles

--- src/components/AboutIconCard.jsx
+++ src/components/AboutIconCard.jsx
@@ -1,6 +1,6 @@
 const AboutIconCard = ({ info }) => {
   return (
-    <div className="flex flex-col items-center gap-x-1 gap-y-2 rounded-lg px-1 py-2 font-semibold text-[#D6C5DC] backdrop-blur-lg dark:text-[#2B2841] sm:gap-x-2 sm:px-2 md:flex-row md:justify-center md:gap-y-0">
+    <div className="flex flex-col items-center gap-x-1 gap-y-2 px-1 py-2 font-semibold text-[#D6C5DC] dark:text-[#2B2841] sm:gap-x-2 sm:px-2 md:flex-row md:justify-center md:gap-y-0">
       {info.icon}
       <div className="flex flex-col">
         <p className="leading-none text-[#5b5662] dark:text-[#D4D3D8]">
