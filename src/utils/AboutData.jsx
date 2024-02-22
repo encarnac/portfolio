20a4cfeaf4324f8eb330b5bc36c1559a7c9ab526
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-22 15:12:04 -0800

    Updated introduction

--- src/utils/AboutData.jsx
+++ src/utils/AboutData.jsx
@@ -4,16 +4,16 @@ export const ABOUT_DATA = {
   introduction: (
     <>
       <p className="text-balance mb-2">
-        I consistently embraced my creativity throughout my life and found joy
-        in the process of designing and crafting things. Due to my background in
-        Psychology, I was also fascinated by the relationship between human
-        behavior and technology, which made learning to code feel like the next
-        natural step.
+        With a background in Psychology, I found myself fascinated by the
+        relationship between human behavior and technology. I also enjoyed
+        designing and creating things through different mediums in my spare
+        time, which made learning to code feel like the next natural step.
       </p>
       <p className="text-balance mb-2">
         My journey as a developer officially began in 2021 when I made a career
-        switch to Computer Science by getting my second Bachelor's degree. Since
-        then, I've continued to grow as a
+        switch to Computer Science by getting my second Bachelor's degree -
+        focusing on front-end, web development, and user experience. Since then,
+        I've continued to grow as a
         <span className="font-bold text-[#DE8ABB] dark:text-[#7943ED]">
           {" "}
           front-end software developer{" "}
