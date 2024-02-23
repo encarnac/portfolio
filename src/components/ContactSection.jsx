4054cf694109351ffbd673dff7f3ce1e361d3360
Colene Encarnado <encarnac@oregonstate.edu>
2024-02-22 16:02:12 -0800

    Moved form to new file and changed to stateful form

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,13 +1,10 @@
 import { useState, useRef } from "react";
 import { motion, useInView } from "framer-motion";
+import ContactForm from "./ContactForm";
 import portrait from "../assets/portrait.png";
 import { FaPhoneAlt, FaEnvelope, FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 const ContactSection = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true, amount: 0.5 });
-
   const contactVariants = {
     hidden: { opacity: 0 },
     visible: {
@@ -139,109 +136,10 @@ const ContactSection = () => {
         </motion.div>
 
         {/* EMAIL FORM SECTION */}
-        <motion.div
-          ref={ref}
-          variants={contactVariants}
-          initial="hidden"
-          animate={isInView ? "visible" : "hidden"}
-          className="col-span-5 mt-8 flex flex-col justify-end md:mt-0"
-        >
-          <form name="contact" method="post">
-            <input type="hidden" name="form-name" value="contact" />
-            <div className="mb-3 flex w-full flex-col items-stretch justify-between gap-x-4 md:flex-row">
-              {/* NAME */}
-              <motion.div
-                variants={contentVariants}
-                className="mb-3 grow md:mb-0"
-              >
-                <label
-                  htmlFor="name"
-                  className="mb-2 block text-sm font-medium"
-                >
-                  Name
-                </label>
-                <input
-                  id="name"
-                  type="text"
-                  name="name"
-                  required
-                  disabled={submitted ? true : false}
-                  className="block w-full rounded-xl border border-[#CCD0D7] bg-[#DCE1E7] p-2.5 text-sm text-[#ADADB6] placeholder-[#9CA2A9] dark:border-[#33353F] dark:bg-[#282638] dark:text-gray-100"
-                  placeholder="John Doe"
-                />
-              </motion.div>
-              {/* EMAIL */}
-              <motion.div variants={contentVariants} className="grow">
-                <label
-                  htmlFor="email"
-                  className="mb-2 block text-sm font-medium"
-                >
-                  Email
-                </label>
-                <input
-                  id="email"
-                  type="email"
-                  name="email"
-                  required
-                  disabled={submitted ? true : false}
-                  className="block w-full rounded-xl border border-[#CCD0D7] bg-[#DCE1E7] p-2.5 text-sm text-[#ADADB6] placeholder-[#9CA2A9] dark:border-[#33353F] dark:bg-[#282638] dark:text-gray-100"
-                  placeholder="johndoe@gmail.com"
-                />
-              </motion.div>
-            </div>
-            {/* SUBJECT */}
-            <motion.div variants={contentVariants} className="mb-3">
-              <label
-                htmlFor="subject"
-                className="mb-2 block text-sm font-medium"
-              >
-                Subject
-              </label>
-              <input
-                id="subject"
-                type="text"
-                name="subject"
-                required
-                disabled={submitted ? true : false}
-                className="block w-full rounded-xl border border-[#CCD0D7] bg-[#DCE1E7] p-2.5 text-sm text-[#ADADB6] placeholder-[#9CA2A9] dark:border-[#33353F] dark:bg-[#282638] dark:text-gray-100"
-                placeholder="Just wanted to say hi"
-              />
-            </motion.div>
-            {/* MESSAGE */}
-            <motion.div variants={contentVariants} className="mb-3">
-              <label
-                htmlFor="message"
-                className="mb-2 block text-sm font-medium"
-              >
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                rows="5"
-                disabled={submitted ? true : false}
-                className="block h-full w-full rounded-xl border border-[#CCD0D7] bg-[#DCE1E7] p-2.5 text-sm text-[#ADADB6] placeholder-[#9CA2A9] dark:border-[#33353F] dark:bg-[#282638] dark:text-gray-100"
-                placeholder="Let's talk about..."
-              />
-            </motion.div>
-            {/* SUBMIT BUTTON */}
-            <motion.button
-              variants={contentVariants}
-              whileHover={{
-                scale: 1.05,
-                transition: { duration: 0.3, ease: "easeInOut" },
-              }}
-              type="submit"
-              className="inline-block w-fit rounded-full bg-gradient-to-br from-[#68A3EB] to-[#F38BBB] px-1 py-1 text-white dark:from-[#3034C2] dark:via-[#6A3DD1] dark:to-[#9329BE]"
-            >
-              <span className="block rounded-full bg-[#e3e8ee] px-10 py-2 transition duration-300 dark:bg-[#201E2C]">
-                <span className="bg-gradient-to-r from-[#68A3EB] to-[#F38BBB] bg-clip-text text-transparent dark:from-[#3034C2] dark:via-[#6A3DD1] dark:to-[#9329BE]">
-                  Submit
-                </span>
-              </span>
-            </motion.button>
-          </form>
-        </motion.div>
+        <ContactForm
+          contactVariants={contactVariants}
+          contentVariants={contentVariants}
+        />
       </div>
     </section>
   );
