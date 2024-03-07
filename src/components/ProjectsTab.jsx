33507292650feb0fe4466e51d5bc7f96969c7b73
Colene Encarnado <encarnac@oregonstate.edu>
2024-03-07 07:05:54 -0800

    Exit animation after tab change

--- src/components/ProjectsTab.jsx
+++ src/components/ProjectsTab.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const ProjectsTab = ({ active, selectFilter, type }) => {
   return (
@@ -13,14 +13,16 @@ const ProjectsTab = ({ active, selectFilter, type }) => {
     >
       <span className="relative">{type} </span>
       <div className="after:absolute after:-bottom-[.22rem] after:left-0 after:h-[.3rem] after:w-0 after:rounded-sm after:bg-[#de8abb] after:opacity-0 after:transition-all after:duration-500 group-hover:after:w-full group-hover:after:opacity-100 after:dark:bg-[#7943ed]"></div>
-
-      {active === type && (
-        <motion.div
-          initial={{ width: "0%" }}
-          className="absolute -bottom-[.22rem] left-0 h-[.3rem] w-full rounded-sm bg-[#de8abb] dark:bg-[#7943ed]"
-          animate={{ width: "100%", transition: { duration: 0.5 } }}
-        ></motion.div>
-      )}
+      <AnimatePresence>
+        {active === type && (
+          <motion.div
+            initial={{ width: "0%" }}
+            animate={{ width: "100%", transition: { duration: 0.3 } }}
+            exit={{ width: "0%", transition: { duration: 0.2 } }}
+            className="absolute -bottom-[.22rem] left-0 h-[.3rem] w-full rounded-sm bg-[#de8abb] dark:bg-[#7943ed]"
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </button>
   );
 };
